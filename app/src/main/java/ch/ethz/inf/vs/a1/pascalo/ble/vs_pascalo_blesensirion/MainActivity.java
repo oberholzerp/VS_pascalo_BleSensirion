4717ecb0f996216cb886331ea9b91a8820e19da6
Joel Busch <joel.busch@hotmail.com>
2016-10-08 14:56:10 +0200

    Added the request for permissions in main

--- app/src/main/java/ch/ethz/inf/vs/a1/pascalo/ble/vs_pascalo_blesensirion/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/pascalo/ble/vs_pascalo_blesensirion/MainActivity.java
@@ -1,11 +1,18 @@
 package ch.ethz.inf.vs.a1.pascalo.ble.vs_pascalo_blesensirion;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private final int MY_PERMISSIONS_REQUEST = 42;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +28,37 @@ public class MainActivity extends AppCompatActivity {
             body.setText(R.string.ble_disabled);
         }
 
+
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)
+                == PackageManager.PERMISSION_DENIED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN)
+                == PackageManager.PERMISSION_DENIED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_DENIED) {
+
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH, Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST);
+        } else {
+
+            Toast.makeText(getApplicationContext(), "Already have permissions", Toast.LENGTH_LONG).show();
+
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults) {
+        if (grantResults[0] == PackageManager.PERMISSION_DENIED
+                || grantResults[1] == PackageManager.PERMISSION_DENIED
+                || grantResults[2] == PackageManager.PERMISSION_DENIED) {
+            Toast.makeText(getApplicationContext(), "Give me permissions you bastard", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Thanks for permissions you bastard", Toast.LENGTH_LONG).show();
+        }
     }
 }
