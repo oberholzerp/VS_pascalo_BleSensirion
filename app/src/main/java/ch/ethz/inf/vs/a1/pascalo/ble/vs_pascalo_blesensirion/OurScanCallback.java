4cef40b2c18cc437dec6132f5e86a2380065318e
Joel Busch <joel.busch@hotmail.com>
2016-10-08 16:17:55 +0200

    added listview to xml added OurScanCallback class, still empty

---
+++ app/src/main/java/ch/ethz/inf/vs/a1/pascalo/ble/vs_pascalo_blesensirion/OurScanCallback.java
@@ -0,0 +1,33 @@
+package ch.ethz.inf.vs.a1.pascalo.ble.vs_pascalo_blesensirion;
+
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+
+import java.util.List;
+
+
+class OurScanCallback extends ScanCallback {
+
+    @Override
+    public void onBatchScanResults (List<ScanResult> results){
+        //Not used, because we use CALLBACK_TYPE_ALL_MATCHES
+    }
+
+    @Override
+    public void onScanFailed (int errorCode) {
+        //TODO: Handle scan fails
+    }
+
+    @Override
+    public void onScanResult (int callbackType, ScanResult result) {
+        if (callbackType == ScanSettings.CALLBACK_TYPE_ALL_MATCHES) {
+
+            // Call into main to add device to listview?
+
+
+        }
+
+
+    }
+}
