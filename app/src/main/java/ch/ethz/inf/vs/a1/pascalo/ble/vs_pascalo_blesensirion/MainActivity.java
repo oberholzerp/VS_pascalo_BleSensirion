2bd04304dc6120e5eab52a66723aaa4aa84e157b
pascalo <pascalo@student.ethz.ch>
2016-10-08 16:29:34 +0200

    enable location service

--- app/src/main/java/ch/ethz/inf/vs/a1/pascalo/ble/vs_pascalo_blesensirion/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/pascalo/ble/vs_pascalo_blesensirion/MainActivity.java
@@ -43,6 +43,8 @@ public class MainActivity
 
     private LocationProvider mLocationProvider;
 
+    private OurScanCallback callback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,6 +87,9 @@ public class MainActivity
             // call enable bluetooth
             enableBT();
 
+            //call enable location service
+            enableLS();
+
         }
 
         ScanFilter.Builder scanFilterBuilder = new ScanFilter.Builder()
@@ -105,6 +110,9 @@ public class MainActivity
 
             // call enable bluetooth
             enableBT();
+
+            //call enable location service
+            enableLS();
         }
     }
 
@@ -130,13 +138,13 @@ public class MainActivity
             public void run() {
                 //mScanning = false;
                 //mBluetoothAdapter.stopLeScan(mLeScanCallback);
-                //mBluetoothLeScanner.stopScan(callback);
+                mBluetoothLeScanner.stopScan(callback);
             }
         }, SCAN_PERIOD);
 
         //mScanning = true;
         //mBluetoothAdapter.startLeScan(mLeScanCallback);
-        //mBluetoothLeScanner.startScan(scanFilters, ScanSettings.CALLBACK_TYPE_ALL_MATCHES, callback);
+        mBluetoothLeScanner.startScan(scanFilters, ScanSettings.CALLBACK_TYPE_ALL_MATCHES, callback);
     }
 
     private void enableBT() {
