53e6587ac08b860b47ebd1f034bd1281a3868baa
Jakob Meier <jakobmeier@bluewin.ch>
2016-10-08 13:57:05 +0200

    Detect BLE support

--- app/src/main/java/ch/ethz/inf/vs/a1/pascalo/ble/vs_pascalo_blesensirion/MainActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/pascalo/ble/vs_pascalo_blesensirion/MainActivity.java
@@ -1,7 +1,9 @@
 package ch.ethz.inf.vs.a1.pascalo.ble.vs_pascalo_blesensirion;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView body = (TextView) findViewById(R.id.body_text);
+
+        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
+            body.setText(R.string.ble_enabled);
+        }
+        else {
+            body.setText(R.string.ble_disabled);
+        }
+
     }
 }
