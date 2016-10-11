fd3f62bea6e19a60a2d0f530c902b73f8ff42bcb
Joel Busch <joel.busch@hotmail.com>
2016-10-11 16:48:18 +0200

    Make parcle and unpack it

--- app/src/main/java/ch/ethz/inf/vs/a1/pascalo/ble/vs_pascalo_blesensirion/GraphActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/pascalo/ble/vs_pascalo_blesensirion/GraphActivity.java
@@ -1,15 +1,25 @@
 package ch.ethz.inf.vs.a1.pascalo.ble.vs_pascalo_blesensirion;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class GraphActivity extends AppCompatActivity {
+
+    private BluetoothDevice mBluetoothDevice;
+    private final String TAG = "GraphActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
+        Intent intent = getIntent();
+        mBluetoothDevice = (BluetoothDevice) intent.getParcelableExtra("BluetoothDevice");
+
+        Log.d(TAG, "You've got mail! Device: " + mBluetoothDevice.toString() + " arrived in GraphActivity");
 
 
     }
